8f5c99405c22e1c6ff4ab8a498b2a918676a275a
SALMAN <elfadili.salman@gmail.com>
2018-12-29 20:37:21 +0100

    Handle InternalAuthenticationServiceException which been thrown after login email is not found

--- src/main/java/com/nearbyshops/security/JWTAuthenticationFilter.java
+++ src/main/java/com/nearbyshops/security/JWTAuthenticationFilter.java
@@ -5,9 +5,13 @@ package com.nearbyshops.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nearbyshops.models.AppUser;
+import com.nearbyshops.services.UserServices;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -28,6 +32,8 @@ import static com.nearbyshops.security.SecurityConstants.*;
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserServices.class);
+
     private AuthenticationManager authenticationManager;
 
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
@@ -41,6 +47,9 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             return authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword()));
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (InternalAuthenticationServiceException e) {
+            logger.error("User email not found!!");
+            return null;
         }
     }
 
